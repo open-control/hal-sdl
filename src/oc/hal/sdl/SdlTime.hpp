1983e7e6522d54be7d20c3037785493a3e736bb7
petitechose-audio <contact@petitechose.audio>
2026-04-27 15:28:25 +0200

    feat(time): register SDL micros provider

--- src/oc/hal/sdl/SdlTime.hpp
+++ src/oc/hal/sdl/SdlTime.hpp
@@ -14,11 +14,22 @@ inline uint32_t defaultTimeProvider() {
     return SDL_GetTicks();
 }
 
+inline uint32_t defaultMicrosProvider() {
+    static const uint64_t start = SDL_GetPerformanceCounter();
+    const uint64_t elapsed = SDL_GetPerformanceCounter() - start;
+    const uint64_t frequency = SDL_GetPerformanceFrequency();
+    if (frequency == 0) {
+        return 0;
+    }
+    return static_cast<uint32_t>((elapsed * 1000000ULL) / frequency);
+}
+
 /**
  * @brief Initialize the global time provider for oc::time functions
  */
 inline void initTime() {
     oc::time::setProvider(defaultTimeProvider);
+    oc::time::setMicrosProvider(defaultMicrosProvider);
 }
 
 } // namespace oc::hal::sdl
